d9365f5ef6ec6721891ce4869e7155028456a101
Ernesto Fatuarte Fernandez <ernesto.fatuarte@gmail.com>
2021-01-12 12:06:48 +0100

    Añadida vista instrucciones y model/crud armas

---
+++ Proyecto_Tema3/src/vistas/instrucciones.java
@@ -0,0 +1,29 @@
+package vistas;
+
+public class instrucciones {
+	
+	public void mostrarInstrucciones() {
+		System.out.println("Desde el principio de los tiempos, los Forajidos cazan al Sheriff y el\r\n"
+				+ "Sheriff caza a los Forajidos. El Renegado planea en secreto, dispuesto a\r\n"
+				+ "ponerse de uno u otro lado.\r\n"
+				+ "¿Pero quiénes son los despiadados Forajidos que quieren coserle a tiros?\r\n"
+				+ "¡Para averiguarlo, tendrás que desenfundar (las cartas, claro)!\r\n"
+				+ "Solo ante el peligro bajo el ardiente sol de mediodía, sucesos inesperados obligan a buscar refugio en una ciudad fantasma.\r\n"
+				+ "En Dodge City, nuevos pistoleros compiten con los clásicos, empleando\r\n"
+				+ "nuevos trucos y técnicas desconocidas.\r\n"
+				+ "Cuando la cosa se pone dura, los más duros ponen toda la carne en el\r\n"
+				+ "asador y se lo juegan todo Por un puñado de cartas.\r\n"
+				+ "¡Y si eso no fuera sufi ciente, pistoleros aún más extraños salen de sus\r\n"
+				+ "escondites!\r\n"
+				+ "Ahora el Sheriff ha decidido zanjar las cosas de una vez por todas, a su\r\n"
+				+ "manera.\r\n"
+				+ "Ha escrito un nombre en una sola bala. Ese nombre es... BANG!");
+		System.out.println("BANG! es un juego de duelos entre un grupo de Forajidos y su objetivo\r\n"
+				+ "principal, el Sheriff, al más puro estilo de los Spaghetti Wésterns. Los\r\n"
+				+ "Alguaciles, de incógnito, ayudan al Sheriff, pero entre todos ellos hay un\r\n"
+				+ "Renegado que persigue sus propios fi nes.\r\n"
+				+ "En BANG! cada jugador interpreta alguno de estos Roles y representa a\r\n"
+				+ "un Personaje inspirado en algún famoso del Salvaje Oeste.");
+	}
+
+}
