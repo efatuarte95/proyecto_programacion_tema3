6810011f8de714ee2b45e020144b13b35e0fdb04
efatuarte95 <ernesto.fatuarte@gmail.com>
2021-01-13 12:27:20 +0100

    Añadida vista instrucciones y llamada en clase Principal

--- Proyecto_Tema3/src/vistas/Instrucciones.java
+++ Proyecto_Tema3/src/vistas/Instrucciones.java
@@ -2,28 +2,32 @@ package vistas;
 
 public class Instrucciones {
 	
-	public void mostrarInstrucciones() {
-		System.out.println("Desde el principio de los tiempos, los Forajidos cazan al Sheriff y el\r\n"
-				+ "Sheriff caza a los Forajidos. El Renegado planea en secreto, dispuesto a\r\n"
-				+ "ponerse de uno u otro lado.\r\n"
-				+ "¿Pero quiénes son los despiadados Forajidos que quieren coserle a tiros?\r\n"
-				+ "¡Para averiguarlo, tendrás que desenfundar (las cartas, claro)!\r\n"
+	public static void mostrarInstrucciones() {
+		System.out.println("\r\n"
+				+ "                                                               \r\n"
+				+ "\t\t88888888ba         db         888b      88    ,ad8888ba,   88  \r\n"
+				+ "\t\t88      \"8b       d88b        8888b     88   d8\"'    `\"8b  88  \r\n"
+				+ "\t\t88      ,8P      d8'`8b       88 `8b    88  d8'            88  \r\n"
+				+ "\t\t88aaaaaa8P'     d8'  `8b      88  `8b   88  88             88  \r\n"
+				+ "\t\t88\"\"\"\"\"\"8b,    d8YaaaaY8b     88   `8b  88  88      88888  88  \r\n"
+				+ "\t\t88      `8b   d8\"\"\"\"\"\"\"\"8b    88    `8b 88  Y8,        88  \"\"  \r\n"
+				+ "\t\t88      a8P  d8'        `8b   88     `8888   Y8a.    .a88  aa  \r\n"
+				+ "\t\t88888888P\"  d8'          `8b  88      `888    `\"Y88888P\"   88  \r\n"
+				+ "\t\t                                                               \r\n"
+				+ "\t\t                                                               \r\n");
+		System.out.println("Desde el principio de los tiempos, los Forajidos cazan al Sheriff y el Sheriff caza a los Forajidos.\n"
+				+ "El Renegado planea en secreto, dispuesto a ponerse de uno u otro lado.\n"
+				+ "¿Pero quiénes son los despiadados Forajidos que quieren coserle a tiros?\n"
+				+ "¡Para averiguarlo, tendrás que desenfundar (las cartas, claro)!\n"
 				+ "Solo ante el peligro bajo el ardiente sol de mediodía, sucesos inesperados obligan a buscar refugio en una ciudad fantasma.\r\n"
-				+ "En Dodge City, nuevos pistoleros compiten con los clásicos, empleando\r\n"
-				+ "nuevos trucos y técnicas desconocidas.\r\n"
-				+ "Cuando la cosa se pone dura, los más duros ponen toda la carne en el\r\n"
-				+ "asador y se lo juegan todo Por un puñado de cartas.\r\n"
-				+ "¡Y si eso no fuera sufi ciente, pistoleros aún más extraños salen de sus\r\n"
-				+ "escondites!\r\n"
-				+ "Ahora el Sheriff ha decidido zanjar las cosas de una vez por todas, a su\r\n"
-				+ "manera.\r\n"
+				+ "En Dodge City, nuevos pistoleros compiten empleando nuevos trucos y técnicas desconocidas.\n"
+				+ "Cuando la cosa se pone dura, los más duros ponen toda la carne en el asador y se lo juegan todo Por un puñado de cartas.\n"
+				+ "¡Y si eso no fuera suficiente, pistoleros aún más extraños salen de sus escondites!\n"
+				+ "Ahora el Sheriff ha decidido zanjar las cosas de una vez por todas, a su manera...\n"
 				+ "Ha escrito un nombre en una sola bala. Ese nombre es... BANG!");
-		System.out.println("BANG! es un juego de duelos entre un grupo de Forajidos y su objetivo\r\n"
-				+ "principal, el Sheriff, al más puro estilo de los Spaghetti Wésterns. Los\r\n"
-				+ "Alguaciles, de incógnito, ayudan al Sheriff, pero entre todos ellos hay un\r\n"
-				+ "Renegado que persigue sus propios fi nes.\r\n"
-				+ "En BANG! cada jugador interpreta alguno de estos Roles y representa a\r\n"
-				+ "un Personaje inspirado en algún famoso del Salvaje Oeste.");
+		System.out.println("BANG! es un juego de duelos entre un grupo de Forajidos y su objetivo principal, el Sheriff, al más puro estilo de los Spaghetti Westerns.\n"
+				+ "Los Alguaciles, de incógnito, ayudan al Sheriff, pero entre todos ellos hay un Renegado que persigue sus propios fines.\n"
+				+ "En BANG! cada jugador interpreta alguno de estos Roles y representa a un Personaje inspirado en algún famoso del Salvaje Oeste.");
 	}
 
 }
