4a60b5ea0096514c23780e656547032a6a11795c
Francisco Gallego Gordillo <frangallego03@gmail.com>
2021-01-14 10:12:39 +0100

    Añado clase BDAcciones

---
+++ Proyecto_Tema3/src/datos/BDAcciones.java
@@ -0,0 +1,17 @@
+package datos;
+
+import model.Acciones;
+
+public class BDAcciones {
+	
+	private Acciones ac1 = new Acciones("Salón", 1, 0, null);
+	private Acciones ac2 = new Acciones("Cerveza", 1, 0, null);
+	private Acciones ac3 = new Acciones("Almacén", 1, 0, null);
+	private Acciones ac4 = new Acciones("Diligencia", 1, 0, null);
+	private Acciones ac5 = new Acciones("Wells Fargo", 1, 0, null);
+	private Acciones ac6 = new Acciones("Ametralladora Gatling", 1, 0, null);
+	private Acciones ac7 = new Acciones("¡Indios!", 1, 0, null);
+	private Acciones ac8 = new Acciones("Duelo", 1, 0, null);
+	
+
+}
